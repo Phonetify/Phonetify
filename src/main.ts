19e7d7705c5e3e3bca0eb2391fe6027813b7b397
Will Lane <williamlane923@gmail.com>
2021-03-13 12:46:30 -0800

    🥇 commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueMaterial)
 
 new Vue({
   router,
