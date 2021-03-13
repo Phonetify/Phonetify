e105c978a0986b750570587030387808beb0a35d
Will Lane <williamlane923@gmail.com>
2021-03-13 14:33:50 -0800

    🛣  added listen route

---
+++ src/views/Listen.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hello world</h1>
+</template>
+
+<script lang="ts">
+
+import {Component, Vue} from "vue-property-decorator";
+
+@Component
+export default class Listen extends Vue {}
+</script>
+
+<style scoped lang="sass"></style>
