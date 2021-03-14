5dd2aba10f41430ec89388a33948ccb4c4dedd4e
Will Lane <williamlane923@gmail.com>
2021-03-13 16:24:29 -0800

    🎨 updated textarea styling and added link input box

--- src/views/Listen.vue
+++ src/views/Listen.vue
@@ -4,9 +4,15 @@
       <button>Back</button>
     </a>
 
-    <label>
-      <textarea class="paste" v-model="pasteText" placeholder="Paste Text"></textarea>
-    </label>
+    <div class="texts">
+      <label>
+        <input type="text" class="link" v-model="linkText" placeholder="Enter Link" />
+      </label>
+
+      <label>
+        <textarea class="paste" v-model="pasteText" placeholder="Paste Text"></textarea>
+      </label>
+    </div>
   </div>
 </template>
 
@@ -21,9 +27,13 @@ import {Component, Vue} from "vue-property-decorator";
     }
   },
 
+  methods: {
+  },
+
   data() {
     return {
-      pasteText: ''
+      pasteText: '',
+      linkText: '',
     }
   }
 })
@@ -56,13 +66,39 @@ export default class Listen extends Vue {}
     &:hover
       cursor: pointer
 
-.paste
-  border: none
-  outline: none
-  border-radius: 15px
-  width: 40vw
-  height: 60vh
-  padding: 2em
+.texts
+  display: flex
+  flex-direction: column
 
-  font-size: 1.3em
+  .paste
+    border: none
+    outline: none
+    border-radius: 15px
+    width: 40vw
+    height: 60vh
+    padding: 1em
+    background-color: #B0BEC5
+    color: #263238
+    font-weight: bold
+    font-family: 'Nunito', sans-serif
+    letter-spacing: 2px
+
+    font-size: 1.3em
+
+  .link
+    width: 40vw
+    border: none
+    outline: none
+    border-radius: 15px
+    padding: 1em
+    background-color: #B0BEC5
+    font-weight: bold
+
+    font-family: 'Nunito', sans-serif
+
+    color: #263238
+    font-size: 1.3em
+
+    letter-spacing: 2px
+    margin-bottom: 1.5em
 </style>
